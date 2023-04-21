f65e8b8d69373a2378aed811b8faa1321eb11a3f
GiangTuan1 <tuanggcc210137@fpt.edu.vn>
2023-04-21 09:28:59 +0700

    test

--- src/login/Register.java
+++ src/login/Register.java
@@ -52,7 +52,7 @@ public class Register extends javax.swing.JFrame {
              System.out.println("Nothing to show");
          }
     }
-
+a
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
